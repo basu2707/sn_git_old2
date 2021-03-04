17f180844c193cf166862a9e5e8424381e3424d6
bigeyes2707 <37492867+bigeyes2707@users.noreply.github.com>
2021-03-04 10:40:00 -0500

    Update pipeline-script-1.groovy

--- pipeline-script-1.groovy
+++ pipeline-script-1.groovy
@@ -6,7 +6,7 @@ pipeline {
     stages {
         stage('preparation') {
             steps {
-                snApplyChanges url: "https://comcastdemo.service-now.com", credentialsId: "snow_git_jenkins_poc", appScope: "global"
+                snApplyChanges url: "https://comcastdemo.service-now.com", credentialsId: "comcastdemo", appScope: "global"
             }
         }
     }
